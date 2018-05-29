2d38875451200815a42ceec785144ff9da2ee90d
arthurmoises <arthurmoises@hotmail.com>
2018-05-29 11:52:37 -0300

    Creating an API to later integrate with Vuejs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MongoClient = require('mongodb').MongoClient;
+var nunjucksRender = require('gulp-nunjucks-render');
+
 
 var db;
 MONGODB_URL = "mongodb://heroku_pj5qws5n:j66hhrtsj1b489346gsmng6s2f@ds231090.mlab.com:31090/heroku_pj5qws5n";
